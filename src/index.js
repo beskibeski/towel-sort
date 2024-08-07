47ed2f4ad5a161a326b5a1f9eb626ec7673613b7
unknown <benladen1917@inbox.ru>
2024-08-08 00:42:09 +0300

    feat: towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = towelSort = (matrix) =>
+   matrix !== undefined && matrix.length > 0
+    ? [...new Set(matrix.map((el, index) => index === 0 || index % 2 === 0 ? el : el.reverse()).flat())]
+    : [];
